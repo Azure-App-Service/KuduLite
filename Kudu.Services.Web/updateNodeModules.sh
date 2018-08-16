7380bb23e9eb4b348fa6265076a245d471906590
Sanchit Mehta <sanchit.mehta602@gmail.com>
2018-08-16 14:00:48 -0700

    Fixed updateNodeModules shell script.Added PublishWithAspNetCoreManifest property to the project file - this will make the ap run even if the SDK is not installed

--- Kudu.Services.Web/updateNodeModules.sh
+++ Kudu.Services.Web/updateNodeModules.sh
@@ -9,12 +9,8 @@
 
 MAX_RETRIES = 5
 
-
-retry "npm install https://github.com/projectkudu/KuduScript/tarball/39c5665a08f2ae50c0f853f5d44f574265ed3118"
-
-
-function retry {
-  local n = 1
+retry() {
+  n = 1
   while true; do
     "$@" && break || {
       if [[ $n -lt $max ]]; then
@@ -29,3 +25,14 @@ function retry {
   done    
 }
 
+copy_to_build_dir() {
+  rm -rf "$@/node_modules"	
+  if [ ! -d "node_modules" ]; then
+    exit 1
+  else 
+    mv node_modules "$@"
+  fi
+}
+
+retry npm install https://github.com/projectkudu/KuduScript/tarball/39c5665a08f2ae50c0f853f5d44f574265ed3118
+copy_to_build_dir "$@"
