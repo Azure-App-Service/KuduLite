c1a3e75e439b56585b8148429eab6cfa30ef29cf
Sanchit Mehta <sanchit.mehta602@gmail.com>
2018-08-28 13:41:38 -0700

    Changing ports

--- Kudu.Services.Web/Program.cs
+++ Kudu.Services.Web/Program.cs
@@ -8,16 +8,11 @@ namespace Kudu.Services.Web
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        //
-        //
-        //
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("http://localhost:1601")
-                .Build();
+                .UseStartup<Startup>();
     }
 }
