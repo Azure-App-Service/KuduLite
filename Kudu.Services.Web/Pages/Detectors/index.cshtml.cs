704f5c72634db09eb9351488430f3e9da12dd2e6
Nick Lucas <nilucas@microsoft.com>
2019-01-07 18:27:40 -0800

    Added detectors page from projectkudu

---
+++ Kudu.Services.Web/Pages/Detectors/index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kudu.Services.Web.Pages.Detectors
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
