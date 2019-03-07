315de0b1be408186eb9b656ae02e08d66e7f7863
JennyLawrance <jennylaw@microsoft.com>
2019-03-06 17:18:58 -0800

    Update python builds to minimize deployment failures'

--- Kudu.Core/Deployment/Generator/OryxBuilder.cs
+++ Kudu.Core/Deployment/Generator/OryxBuilder.cs
@@ -46,6 +46,18 @@ namespace Kudu.Core.Deployment.Generator
             {
                 oryxLanguage = "python";
                 runOryxBuild = true;
+                string virtualEnvName = "antenv";
+
+                if (version.StartsWith("3.6"))
+                {
+                    virtualEnvName = "antenv3.6";
+                }
+                else if (version.StartsWith("2.7"))
+                {
+                    virtualEnvName = "antenv2.7";
+                }
+
+                additionalOptions = string.Format("-p virtualenv_name={0}", virtualEnvName);
             }
 
             if (runOryxBuild)
