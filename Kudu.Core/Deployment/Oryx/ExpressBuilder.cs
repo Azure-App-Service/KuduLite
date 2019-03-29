7ba5e9e4c7be33ea7c9c869e304ece9e14d35f6b
JennyLawrance <jennylaw@microsoft.com>
2019-03-29 11:52:47 -0700

    Test Commit for new build options

---
+++ Kudu.Core/Deployment/Oryx/ExpressBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kudu.Core.Deployment.Oryx
+{
+    public class ExpressBuilder
+    {
+        public void SetupExpressBuilderArtifacts()
+        {
+            // 0. Generate the packagename.txt:
+            // 1. Copy the zip'd output to /home/data/SitePackages
+        }
+    }
+}
