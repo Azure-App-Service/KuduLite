1647adf5b5c47107b7a8e7d39a9c2871faa2e540
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-09-11 14:21:51 -0700

    Add logger for dependencies

--- Kudu.Core/Helpers/LinuxConsumptionDeploymentHelper.cs
+++ Kudu.Core/Helpers/LinuxConsumptionDeploymentHelper.cs
@@ -1,14 +1,18 @@
-﻿using Kudu.Core.Deployment;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Microsoft.WindowsAzure.Storage.Blob;
-using Microsoft.WindowsAzure.Storage;
+﻿using Kudu.Contracts.Settings;
+using Kudu.Core.Deployment;
+using Kudu.Core.Deployment.Generator;
 using Kudu.Core.Deployment.Oryx;
 using Kudu.Core.Infrastructure;
-using Kudu.Contracts.Settings;
+using Kudu.Core.Tracing;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
 using System.IO;
-using Kudu.Core.Deployment.Generator;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Kudu.Core.Helpers
 {
@@ -28,6 +32,9 @@ namespace Kudu.Core.Helpers
             // Package built content from oryx build artifact
             string filePath = PackageArtifactFromFolder(env, settings, context, builtFolder, packageFolder, packageFileName);
 
+            // Log function app dependencies to kusto (requirements.txt, package.json, .csproj)
+            await LogDependenciesFile(context.RepositoryPath);
+
             // Upload from DeploymentsPath
             await UploadLinuxConsumptionFunctionAppBuiltContent(context, sas, filePath);
 
@@ -38,7 +45,134 @@ namespace Kudu.Core.Helpers
             await RemoveLinuxConsumptionFunctionAppWorkers(context);
         }
 
-        public static async Task UploadLinuxConsumptionFunctionAppBuiltContent(DeploymentContext context, string sas, string filePath)
+        private static async Task LogDependenciesFile(string builtFolder)
+        {
+            try
+            {
+                await PrintRequirementsTxtDependenciesAsync(builtFolder);
+                await PrintPackageJsonDependenciesAsync(builtFolder);
+                PrintCsprojDependenciesAsync(builtFolder);
+            } catch (Exception)
+            {
+                KuduEventGenerator.Log().GenericEvent(
+                    ServerConfiguration.GetApplicationName(),
+                    $"dependencies,failed to parse function app dependencies",
+                    Guid.Empty.ToString(),
+                    string.Empty,
+                    string.Empty,
+                    string.Empty);
+            }
+        }
+
+        private static async Task PrintRequirementsTxtDependenciesAsync(string builtFolder)
+        {
+            string filename = "requirements.txt";
+            string requirementsTxtPath = Path.Combine(builtFolder, filename);
+            if (File.Exists(requirementsTxtPath))
+            {
+                string[] lines = await File.ReadAllLinesAsync(requirementsTxtPath);
+                foreach (string line in lines)
+                {
+                    int separatorIndex;
+                    if (line.IndexOf("==") >= 0)
+                    {
+                        separatorIndex = line.IndexOf("==");
+                    } else if (line.IndexOf(">=") >= 0)
+                    {
+                        separatorIndex = line.IndexOf(">=");
+                    } else if (line.IndexOf("<=") >= 0)
+                    {
+                        separatorIndex = line.IndexOf("<=");
+                    } else if (line.IndexOf(">") >= 0)
+                    {
+                        separatorIndex = line.IndexOf(">");
+                    } else if (line.IndexOf("<") >= 0)
+                    {
+                        separatorIndex = line.IndexOf("<");
+                    } else
+                    {
+                        separatorIndex = line.Length;
+                    }
+
+                    string package = line.Substring(0, separatorIndex).Trim();
+                    string version = line.Substring(separatorIndex).Trim();
+
+                    KuduEventGenerator.Log().GenericEvent(
+                        ServerConfiguration.GetApplicationName(),
+                        $"dependencies,python,{filename},{package},{version}",
+                        Guid.Empty.ToString(),
+                        string.Empty,
+                        string.Empty,
+                        string.Empty);
+                }
+            }
+        }
+
+        private static async Task PrintPackageJsonDependenciesAsync(string builtFolder)
+        {
+            string filename = "package.json";
+            string packageJsonPath = Path.Combine(builtFolder, filename);
+            if (File.Exists(packageJsonPath))
+            {
+                string content = await File.ReadAllTextAsync(packageJsonPath);
+                JObject jobj = JObject.Parse(content);
+                if (jobj.ContainsKey("devDependencies"))
+                {
+                    Dictionary<string, string> dictObj = jobj["devDependencies"].ToObject<Dictionary<string, string>>();
+                    foreach (string key in dictObj.Keys)
+                    {
+                        KuduEventGenerator.Log().GenericEvent(
+                            ServerConfiguration.GetApplicationName(),
+                            $"dependencies,node,{filename},{key},{dictObj[key]},devDependencies",
+                            Guid.Empty.ToString(),
+                            string.Empty,
+                            string.Empty,
+                            string.Empty);
+                    }
+                }
+
+                if (jobj.ContainsKey("dependencies"))
+                {
+                    Dictionary<string, string> dictObj = jobj["dependencies"].ToObject<Dictionary<string, string>>();
+                    foreach (string key in dictObj.Keys)
+                    {
+                        KuduEventGenerator.Log().GenericEvent(
+                            ServerConfiguration.GetApplicationName(),
+                            $"dependencies,node,{filename},{key},{dictObj[key]},dependencies",
+                            Guid.Empty.ToString(),
+                            string.Empty,
+                            string.Empty,
+                            string.Empty);
+                    }
+                }
+            }
+        }
+
+        private static void PrintCsprojDependenciesAsync(string builtFolder)
+        {
+            foreach (string csprojPath in Directory.GetFiles(builtFolder, "*.csproj", SearchOption.TopDirectoryOnly))
+            {
+                string filename = Path.GetFileName(csprojPath);
+                XElement purchaseOrder = XElement.Load(csprojPath);
+                foreach (var itemGroup in purchaseOrder.Elements("ItemGroup"))
+                {
+                    foreach (var packageReference in itemGroup.Elements("PackageReference"))
+                    {
+                        string include = packageReference.Attribute("Include").Value;
+                        string version = packageReference.Attribute("Version").Value;
+                        KuduEventGenerator.Log().GenericEvent(
+                            ServerConfiguration.GetApplicationName(),
+                            $"dependencies,dotnet,{filename},{include},{version}",
+                            Guid.Empty.ToString(),
+                            string.Empty,
+                            string.Empty,
+                            string.Empty);
+                    }
+                }
+            }
+        }
+
+        private static async Task UploadLinuxConsumptionFunctionAppBuiltContent(DeploymentContext context, string sas, string filePath)
         {
             context.Logger.Log($"Uploading built content {filePath} -> {sas}");
 
@@ -71,7 +205,7 @@ namespace Kudu.Core.Helpers
             }
         }
 
-        public static async Task RemoveLinuxConsumptionFunctionAppWorkers(DeploymentContext context)
+        private static async Task RemoveLinuxConsumptionFunctionAppWorkers(DeploymentContext context)
         {
             string webSiteHostName = System.Environment.GetEnvironmentVariable(SettingsKeys.WebsiteHostname);
             string sitename = ServerConfiguration.GetApplicationName();
