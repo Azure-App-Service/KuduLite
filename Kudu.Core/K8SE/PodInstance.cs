dd57da128b6b1f6f6fe3168cea346afe0b7c9009
Sanchit Mehta <sanchit.mehta602@gmail.com>
2020-11-30 01:40:22 -0800

    Added Instance support

---
+++ Kudu.Core/K8SE/PodInstance.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Kudu.Core.K8SE
+{
+    public class PodInstance
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "ipAddr")]
+        public string IpAddress { get; set; }
+
+        [JsonProperty(PropertyName = "nodeName")]
+        public string NodeName { get; set; }
+
+        [JsonProperty(PropertyName = "startTime")]
+        public string StartTime { get; set; }
+    }
+}
