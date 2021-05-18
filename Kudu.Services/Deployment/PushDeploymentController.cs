beb67fc5a0fae694c70e1f270d4b41afff2781d2
Satish Ranjan <satishra@microsoft.com>
2021-05-18 12:24:00 -0700

    Fixing the code to extract packageUri from the ARM payload

--- Kudu.Services/Deployment/PushDeploymentController.cs
+++ Kudu.Services/Deployment/PushDeploymentController.cs
@@ -762,7 +762,7 @@ namespace Kudu.Services.Deployment
                 try
                 {
                     // ARM template should have properties field and a packageUri field inside the properties field.
-                    string packageUri = requestObject.Value<JObject>("properties").Value<string>("packageUri");
+                    string packageUri = requestObject.Value<string>("packageUri");
                     if (string.IsNullOrEmpty(packageUri))
                     {
                         throw new ArgumentException("Invalid Url in the JSON request");
