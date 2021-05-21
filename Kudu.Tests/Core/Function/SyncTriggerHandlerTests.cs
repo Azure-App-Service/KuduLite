47fe198efa4596a04cf331c3bdcef48d5d1ac443
Chris Gillum <cgillum@microsoft.com>
2021-05-20 18:04:13 -0700

    Unit test for Durable Functinos syncTriggers

--- Kudu.Tests/Core/Function/SyncTriggerHandlerTests.cs
+++ Kudu.Tests/Core/Function/SyncTriggerHandlerTests.cs
@@ -1,5 +1,8 @@
-﻿using Kudu.Core.Functions;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Kudu.Core.Functions;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xunit;
 
@@ -23,6 +26,30 @@ namespace Kudu.Tests.Core.Function
             {
                 Assert.True(scaleTriggers != null && scaleTriggers.Item1 != null && scaleTriggers.Item1.ToList<ScaleTrigger>().Count > 0);
             }
+        }
+
+        [Fact]
+        public void DurableFunctions_SyncTriggers()
+        {
+            // NOTE: Same basic inputs as KedaFunctionTriggersProviderTests.DurableFunctionsApp
+            JObject syncTriggersPayloadJson = new JObject(
+                new JProperty("extensions", JObject.Parse(@"{""durableTask"":{""hubName"":""DFTest"",""storageProvider"":{""type"":""mssql"",""connectionStringName"":""SQLDB_Connection""}}}")),
+                new JProperty("triggers", new JArray(
+                    JObject.Parse(@"{""functionName"":""f1"",""type"":""orchestrationTrigger"",""name"":""context""}"),
+                    JObject.Parse(@"{""functionName"":""f2"",""type"":""entityTrigger"",""name"":""ctx""}"),
+                    JObject.Parse(@"{""functionName"":""f3"",""type"":""activityTrigger"",""name"":""input""}"),
+                    JObject.Parse(@"{""functionName"":""f4"",""type"":""queueTrigger"",""connection"":""AzureWebjobsStorage"",""queueName"":""queue"",""name"":""queueItem""}"),
+                    JObject.Parse(@"{""functionName"":""f5"",""type"":""httpTrigger"",""methods"":[""post""],""authLevel"":""anonymous"",""name"":""req""}"))));
+
+            string serializedPayload = syncTriggersPayloadJson.ToString(Formatting.None);
+
+            var syncTriggerHandler = new SyncTriggerHandler(null, null, null);
+            (IEnumerable<ScaleTrigger> triggers, string error) = syncTriggerHandler.GetScaleTriggers(serializedPayload);
+            Assert.Null(error);
+            Assert.NotNull(triggers);
+
+            // We expect the same output as the other Durable Functions test
+            KedaFunctionTriggersProviderTests.ValidateDurableTriggers(triggers);
         }
     }
 }
