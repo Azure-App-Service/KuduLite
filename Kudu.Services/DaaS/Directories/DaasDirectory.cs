9a0b68a7e02f5b518d756cd16736d917b7c24cce
Puneet Gupta <puneetg@microsoft.com>
2021-07-15 15:34:37 +0530

    correcting paths

--- Kudu.Services/DaaS/Directories/DaasDirectory.cs
+++ Kudu.Services/DaaS/Directories/DaasDirectory.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.IO;
 
 namespace Kudu.Services.Performance
 {
     internal class DaasDirectory
     {
-        protected static readonly string daasPath = Environment.ExpandEnvironmentVariables(@"%HOME%\Data\DaaS");
+        protected static readonly string daasPath = Path.Combine(Environment.ExpandEnvironmentVariables(@"%HOME%"), "Data", "DaaS");
         protected static readonly string daasRelativePath = @"/Data/DaaS";
     }
 }
