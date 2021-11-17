4f84cc937683b7f21940406e5a3bf4272a65aa77
Layla Liu <layliu@microsoft.com>
2021-11-17 17:52:44 +0800

    Enable per site per environment lock

--- Kudu.Services.Web/KuduWebUtil.cs
+++ Kudu.Services.Web/KuduWebUtil.cs
@@ -29,14 +29,15 @@ using Org.BouncyCastle.Asn1.Ocsp;
 
 namespace Kudu.Services.Web
 {
-    internal static class KuduWebUtil
+    public static class KuduWebUtil
     {
         private const string KuduConsoleFilename = "kudu.dll";
         private const string KuduConsoleRelativePath = "KuduConsole";
+        private const string GlobalLockKey = "@@Global"; //Key for the global lock in namedSiteLocks
 
-        private static Dictionary<string, IOperationLock> _namedLocks;
-
-        private static DeploymentLockFile _deploymentLock;
+        //private static Dictionary<string, IOperationLock> _namedLocks;
+        private static IDictionary<string, IDictionary<string, IOperationLock>> namedSiteLocks;
+        private static object lockObj = new object();
 
         // <summary>
         // This method initializes status,ssh,hooks & deployment locks used by Kudu to ensure
@@ -76,13 +77,40 @@ namespace Kudu.Services.Web
         // </remarks>
         private static void SetupLocks(ITraceFactory traceFactory, IEnvironment environment)
         {
+            var _namedLocks = CreateLocks(traceFactory, environment);
+            var key = GetNamedLocksKey(environment);
+
+            if (namedSiteLocks == null)
+            {
+                lock (lockObj)
+                {
+                    namedSiteLocks = new Dictionary<string, IDictionary<string, IOperationLock>>()
+                    {
+                        { key, _namedLocks}
+                    };
+                }
+            }
+            else
+            {
+                lock (lockObj)
+                {   
+                    if (!namedSiteLocks.ContainsKey(key))
+                    {
+                        namedSiteLocks.Add(key, _namedLocks);
+                    }
+                }
+            }
+        }
+
+        private static Dictionary<string, IOperationLock> CreateLocks(ITraceFactory traceFactory, IEnvironment environment)
+        {
             var lockPath = Path.Combine(environment.SiteRootPath, Constants.LockPath);
             var deploymentLockPath = Path.Combine(lockPath, Constants.DeploymentLockFile);
             var statusLockPath = Path.Combine(lockPath, Constants.StatusLockFile);
             var sshKeyLockPath = Path.Combine(lockPath, Constants.SSHKeyLockFile);
             var hooksLockPath = Path.Combine(lockPath, Constants.HooksLockFile);
-            _deploymentLock = DeploymentLockFile.GetInstance(deploymentLockPath, traceFactory);
-            _deploymentLock.InitializeAsyncLocks();
+            var deploymentLock = DeploymentLockFile.GetInstance(deploymentLockPath, traceFactory);
+            deploymentLock.InitializeAsyncLocks();
 
             var statusLock = new LockFile(statusLockPath, traceFactory);
             statusLock.InitializeAsyncLocks();
@@ -91,15 +119,19 @@ namespace Kudu.Services.Web
             var hooksLock = new LockFile(hooksLockPath, traceFactory);
             hooksLock.InitializeAsyncLocks();
 
-            _namedLocks = new Dictionary<string, IOperationLock>
+            return new Dictionary<string, IOperationLock>
             {
-                {Constants.StatusLockName, statusLock},
-                {Constants.SshLockName, sshKeyLock},
-                {Constants.HooksLockName, hooksLock},
-                {Constants.DeploymentLockName, _deploymentLock}
+                { Constants.StatusLockName, statusLock},
+                { Constants.SshLockName, sshKeyLock},
+                { Constants.HooksLockName, hooksLock},
+                { Constants.DeploymentLockName, deploymentLock}
             };
         }
 
+        private static string GetNamedLocksKey(IEnvironment environment)
+        {
+            return environment.GetNormalizedK8SEAppName() ?? GlobalLockKey;
+        }
 
         internal static void SetupFileServer(IApplicationBuilder app, string fileDirectoryPath, string requestPath)
         {
@@ -470,14 +502,11 @@ namespace Kudu.Services.Web
         /// <param name="traceFactory"></param>
         /// <param name="environment"></param>
         /// <returns></returns>
-        internal static DeploymentLockFile GetDeploymentLock(ITraceFactory traceFactory, IEnvironment environment)
+        public static DeploymentLockFile GetDeploymentLock(ITraceFactory traceFactory, IEnvironment environment)
         {
-            if (_namedLocks == null || _deploymentLock == null)
-            {
-                GetNamedLocks(traceFactory, environment);
-            }
+            var namedLocks = GetNamedLocks(traceFactory, environment);
 
-            return _deploymentLock;
+            return namedLocks[Constants.DeploymentLockName] as DeploymentLockFile;
         }
 
         /// <summary>
@@ -487,15 +516,12 @@ namespace Kudu.Services.Web
         /// <param name="traceFactory"></param>
         /// <param name="environment"></param>
         /// <returns></returns>
-        internal static Dictionary<string, IOperationLock> GetNamedLocks(ITraceFactory traceFactory,
+        public static IDictionary<string, IOperationLock> GetNamedLocks(ITraceFactory traceFactory,
             IEnvironment environment)
         {
-            if (_namedLocks == null)
-            {
-                SetupLocks(traceFactory, environment);
-            }
+            SetupLocks(traceFactory, environment);
 
-            return _namedLocks;
+            return namedSiteLocks[GetNamedLocksKey(environment)];
         }
 
         // <summary>
---
+++ Kudu.Tests/Services.Web/KuduWebUtilTests.cs
@@ -0,0 +1,168 @@
+﻿using Kudu.Contracts.Infrastructure;
+using Kudu.Core.Tracing;
+using Kudu.Services.Web;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Kudu.Tests.Services
+{
+    //File delete cannot be finished immediately
+    public class KuduWebUtilTests
+    {
+        [Fact(Skip = "only for local run")]
+        public void TestNamedSiteLockWithPerAppEnv_False_False()
+        {
+            TestNamedSiteLockWithPerAppEnv("FALSE", "FALSE");
+            TestNamedSiteLockWithPerAppEnv("false", "False");
+            TestNamedSiteLockSuccess("false", "false");
+        }
+
+        [Fact(Skip = "only for local run")]
+        public void TestNamedSiteLockWithPerAppEnv_False_True()
+        {
+            TestNamedSiteLockWithPerAppEnv("FALSE", "True");
+            TestNamedSiteLockWithPerAppEnv("false", "TRUE");
+            TestNamedSiteLockSuccess("false", "TRUE");
+        }
+
+        [Fact(Skip = "only for local run")]
+        public void TestNamedSiteLockWithPerAppEnv_True_False()
+        {
+            TestNamedSiteLockWithPerAppEnv("true", "FALSE");
+            TestNamedSiteLockWithPerAppEnv("TRUE", "False");
+            TestNamedSiteLockSuccess("TRUE", "False");
+        }
+
+        [Fact(Skip = "only for local run")]
+        public void TestNamedSiteLockWithPerAppEnv_True_True()
+        {
+            TestNamedSiteLockWithPerAppEnv("TRUE", "TRUE");
+            TestNamedSiteLockWithPerAppEnv("True", "true");
+            TestNamedSiteLockSuccess("True", "true");
+        }
+
+        private void TestNamedSiteLockWithPerAppEnv(string isBuildJob, string useBuildJob)
+        {
+            var mockedEnvironment = new Dictionary<string, string>()
+            {
+                { "IS_BUILD_JOB", isBuildJob},
+                { "USE_BUILD_JOB",useBuildJob }
+            };
+
+            bool usePerSiteLock = string.Equals(isBuildJob, "true", System.StringComparison.OrdinalIgnoreCase)
+                || string.Equals(useBuildJob, "true", System.StringComparison.OrdinalIgnoreCase);
+
+            ITraceFactory traceFactory = NullTracerFactory.Instance;
+            var environment1 = TestMockedEnvironment.GetMockedEnvironment(appName: "testApp");
+            var environment2 = TestMockedEnvironment.GetMockedEnvironment(appName: "testApp2");
+            using (new TestScopedEnvironmentVariable(mockedEnvironment))
+            {
+                var deploymentLock = KuduWebUtil.GetDeploymentLock(traceFactory, environment1);
+
+                deploymentLock.LockOperation(() =>
+                {
+                    var deploymentLock2 = KuduWebUtil.GetDeploymentLock(traceFactory, environment2);
+                    var lockResult = deploymentLock2.TryLockOperation(() =>
+                    {
+                        
+                    }, "deploymentLockAgain", new System.TimeSpan(0, 0, 2));
+
+                    Assert.True(usePerSiteLock && lockResult || !usePerSiteLock && !lockResult);
+
+                }, "deploymentLock", new System.TimeSpan(0, 0, 30));
+            }
+        }
+
+        [Fact(Skip = "only for local run")]
+        public void TestNamedSiteLockWithGlobalEnv_False_False()
+        {
+            TestNamedSiteLockWithGlobalEnv("FALSE", "FALSE");
+            TestNamedSiteLockWithGlobalEnv("false", "False");
+            TestNamedSiteLockSuccess("false", "false", true);
+        }
+
+        [Fact(Skip = "only for local run")]
+        public void TestNamedSiteLockWithGlobalEnv_False_True()
+        {
+            TestNamedSiteLockWithGlobalEnv("FALSE", "True");
+            TestNamedSiteLockWithGlobalEnv("false", "TRUE");
+            TestNamedSiteLockSuccess("false", "TRUE", true);
+        }
+
+        [Fact(Skip = "only for local run")]
+        public void TestNamedSiteLockWithGlobalEnv_True_False()
+        {
+            TestNamedSiteLockWithGlobalEnv("true", "FALSE");
+            TestNamedSiteLockWithGlobalEnv("TRUE", "False");
+            TestNamedSiteLockSuccess("TRUE", "False", true);
+        }
+
+        [Fact(Skip = "only for local run")]
+        public void TestNamedSiteLockWithGlobalEnv_True_True()
+        {
+            TestNamedSiteLockWithGlobalEnv("TRUE", "TRUE");
+            TestNamedSiteLockWithGlobalEnv("True", "true");
+            TestNamedSiteLockSuccess("True", "true", true);
+        }
+
+        private void TestNamedSiteLockWithGlobalEnv(string isBuildJob, string useBuildJob)
+        {
+            var mockedEnvironment = new Dictionary<string, string>()
+            {
+                { "IS_BUILD_JOB", isBuildJob},
+                { "USE_BUILD_JOB",useBuildJob }
+            };
+
+            bool usePerSiteLock = string.Equals(isBuildJob, "true", System.StringComparison.OrdinalIgnoreCase)
+                || string.Equals(useBuildJob, "true", System.StringComparison.OrdinalIgnoreCase);
+
+            ITraceFactory traceFactory = NullTracerFactory.Instance;
+            var environment1 = TestMockedEnvironment.GetMockedEnvironment();
+            var environment2 = TestMockedEnvironment.GetMockedEnvironment();
+            using (new TestScopedEnvironmentVariable(mockedEnvironment))
+            {
+                var deploymentLock = KuduWebUtil.GetDeploymentLock(traceFactory, environment1);
+
+                deploymentLock.LockOperation(() =>
+                {
+                    var deploymentLock2 = KuduWebUtil.GetDeploymentLock(traceFactory, environment2);
+                    var lockResult = deploymentLock2.TryLockOperation(() =>
+                    {
+
+                    }, "deploymentLockAgain", new System.TimeSpan(0, 0, 2));
+
+                    Assert.False(lockResult);
+
+                }, "deploymentLock", new System.TimeSpan(0, 0, 30));
+            }
+        }
+
+        private void TestNamedSiteLockSuccess(string isBuildJob, string useBuildJob, bool global = false)
+        {
+            var mockedEnvironment = new Dictionary<string, string>()
+            {
+                { "IS_BUILD_JOB", isBuildJob},
+                { "USE_BUILD_JOB",useBuildJob }
+            };
+
+            ITraceFactory traceFactory = NullTracerFactory.Instance;
+            var environment1 = TestMockedEnvironment.GetMockedEnvironment(appName: global ? null : "testApp");
+            var environment2 = TestMockedEnvironment.GetMockedEnvironment(appName: global ? null : "testApp2");
+            using (new TestScopedEnvironmentVariable(mockedEnvironment))
+            {
+                var deploymentLock = KuduWebUtil.GetDeploymentLock(traceFactory, environment1);
+
+                deploymentLock.LockOperation(() =>
+                {
+                    var deploymentLock2 = KuduWebUtil.GetDeploymentLock(traceFactory, environment2);
+                    var lockResult = deploymentLock.TryLockOperation(() =>
+                    {
+                    }, "deploymentLockAgain", new System.TimeSpan(0, 0, 10));
+
+                    Assert.True(lockResult);
+
+                }, "deploymentLock", new System.TimeSpan(0, 0, 3));
+            }
+        }
+    }
+}
